0920fa5d3683f3eacbc0809f5301bdb1de16a4a6
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-09-15 15:14:59 +0800

    add requirement.txt

--- tools/auto.py
+++ tools/auto.py
@@ -1,21 +1,29 @@
 import os
 import json
+import platform
+
+
 def get_pdf_file_path(path):
+    system = platform.system()
     now_path = os.getcwd()
     with open(path) as s:
         pdf_folder = json.load(s)
     file_list = os.listdir(pdf_folder["pdf_path"])
     pdf_list = []
-    for x in file_list :
-        if ".pdf" in x :
-            path = now_path + "\\" + pdf_folder["pdf_path"][2:] + "\\" + x
+    for x in file_list:
+        if ".pdf" in x:
+            if system == "Windows":
+                path = now_path + "\\" + pdf_folder["pdf_path"][2:] + "\\" + x
+            elif system == "Linux":
+                path = now_path + "/" + pdf_folder["pdf_path"][2:] + "/" + x
             pdf_list.append(path)
     return pdf_list
+
 
 def get_word_file_path(path):
     pdf = get_pdf_file_path(path)
     word_list = []
     for x in pdf:
-        word = x.replace(".pdf",".docx").replace("pdf","word")
+        word = x.replace(".pdf", ".docx").replace("pdf", "word")
         word_list.append(word)
     return word_list
